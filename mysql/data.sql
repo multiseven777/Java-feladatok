33c276f0416d8401d64891d8dd7fb4cdaf3535d2
multiseven777 <porike90@gmail.com>
2017-02-15 12:08:23 +0100

    rearranged data structure

--- mysql/data.sql
+++ mysql/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO template(movie_name, movie_release, movie_genre) VALUES ('Seven', '1997', 'thriller');
-INSERT INTO template(movie_name, movie_release, movie_genre) VALUES ('Fight Club', '1999', 'thriller');
-INSERT INTO template(movie_name, movie_release, movie_genre) VALUES ('Deadpool', '2016', 'comic');
-INSERT INTO template(movie_name, movie_release, movie_genre) VALUES ('The Interview', '2015', 'comedy');
+INSERT INTO movie_database(name_m, release_m, genre_m, lead_actor, imdb_rating, user_rating) VALUES ('Seven', '1997', 'thriller', 'Bradd Pitt', 9, 9);
+INSERT INTO movie_database(name_m, release_m, genre_m, lead_actor, imdb_rating, user_rating) VALUES ('Fight Club', '1999', 'thriller', 'Edward Norton, Brad Pitt', 9, 9);
+INSERT INTO movie_database(name_m, release_m, genre_m, lead_actor, imdb_rating, user_rating) VALUES ('Deadpool', '2016', 'comic', 'Ryan Reynolds', 8, 8);
+INSERT INTO movie_database(name_m, release_m, genre_m, lead_actor, imdb_rating, user_rating) VALUES ('The Interview', '2015', 'comedy', 'James Franco, Seth Rogan', 7, 9);
 
-INSERT INTO dvd(name_m, release_m, lead_actor, imdb_rating, user_rating) values((select movie_name from template where movie_name = 'Seven'), (select movie_release from template where movie_name = 'Seven'), 'Bradd Pitt', 9, 9);
-INSERT INTO dvd(name_m, release_m, lead_actor, imdb_rating, user_rating) values((select movie_name from template where movie_name = 'Fight Club'), (select movie_release from template where movie_name = 'Fight Club'), 'Edward Norton, Brad Pitt', 9, 9);
-INSERT INTO dvd(name_m, release_m, lead_actor, imdb_rating, user_rating) values((select movie_name from template where movie_name = 'Deadpool'), (select movie_release from template where movie_name = 'Deadpool'), 'Ryan Reynolds', 8, 8);
-INSERT INTO dvd(name_m, release_m, lead_actor, imdb_rating, user_rating) values((select movie_name from template where movie_name = 'The Interview'), (select movie_release from template where movie_name = 'The Interview'), 'James Franco, Seth Rogan', 7, 9);
+INSERT INTO movie_collection(movie_name, movie_release) values((select name_m from movie_database where name_m = 'Seven'), (select release_m from movie_database where name_m = 'Seven'));
+INSERT INTO movie_collection(movie_name, movie_release) values((select name_m from movie_database where name_m = 'Fight Club'), (select release_m from movie_database where name_m = 'Fight Club'));
+INSERT INTO movie_collection(movie_name, movie_release) values((select name_m from movie_database where name_m = 'Deadpool'), (select release_m from movie_database where name_m = 'Deadpool'));
+INSERT INTO movie_collection(movie_name, movie_release) values((select name_m from movie_database where name_m = 'The Interview'), (select release_m from movie_database where name_m = 'The Interview'));
--- mysql/schema.sql
+++ mysql/schema.sql
@@ -7,51 +7,69 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema movies
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `movies` ;
+
+-- -----------------------------------------------------
+-- Schema movies
+-- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `movies` ;
 USE `movies` ;
 
-SELECT CONCAT('DROP TABLES IF EXIST ',
- template,
- dvd, ';')
-
-FROM
-    information_schema_tables
-
-WHERE
-    table_schema = 'movies'
-
 -- -----------------------------------------------------
--- Table `movies`.`template`
+-- Table `movies`.`movie_database`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `movies`.`template` (
-  `movie_name` VARCHAR(45) NOT NULL,
-  `movie_release` YEAR NOT NULL,
-  `movie_genre` VARCHAR(45) NULL,
-  PRIMARY KEY (`movie_name`, `movie_release`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+DROP TABLE IF EXISTS `movies`.`movie_database` ;
 
-
--- -----------------------------------------------------
--- Table `movies`.`dvd`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `movies`.`dvd` (
-  `dvd_id` INT NOT NULL AUTO_INCREMENT,
-  `name_m` VARCHAR(45) NOT NULL,
-  `release_m` YEAR NOT NULL,
+CREATE TABLE IF NOT EXISTS `movies`.`movie_database` (
+  `coll_id` INT NOT NULL AUTO_INCREMENT,
+  `name_m` VARCHAR(45) NULL,
+  `release_m` YEAR NULL,
+  `genre_m` VARCHAR(45) NULL,
   `lead_actor` VARCHAR(45) NULL,
   `imdb_rating` INT NULL,
   `user_rating` INT NULL,
-  PRIMARY KEY (`dvd_id`, `name_m`, `release_m`),
-  INDEX `fk_dvd_template_idx` (`name_m` ASC, `release_m` ASC),
-  CONSTRAINT `fk_dvd_template`
-    FOREIGN KEY (`name_m` , `release_m`)
-    REFERENCES `movies`.`template` (`movie_name` , `movie_release`)
-    ON DELETE NO ACTION
-    ON UPDATE CASCADE)
+  PRIMARY KEY (`coll_id`),
+  UNIQUE INDEX `name_m_UNIQUE` (`name_m` ASC),
+  UNIQUE INDEX `release_m_UNIQUE` (`release_m` ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
+
+-- -----------------------------------------------------
+-- Table `movies`.`movie_collection`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `movies`.`movie_collection` ;
+
+CREATE TABLE IF NOT EXISTS `movies`.`movie_collection` (
+  `movie_id` INT NOT NULL AUTO_INCREMENT,
+  `movie_name` VARCHAR(45) NULL,
+  `movie_release` YEAR NULL,
+  PRIMARY KEY (`movie_id`),
+  INDEX `fk_movie_collection_movie_database_idx` (`movie_name` ASC),
+  INDEX `fk_movie_collection_movie_database1_idx` (`movie_release` ASC),
+  UNIQUE INDEX `movie_name_UNIQUE` (`movie_name` ASC),
+  UNIQUE INDEX `movie_release_UNIQUE` (`movie_release` ASC),
+  CONSTRAINT `fk_movie_collection_movie_database`
+    FOREIGN KEY (`movie_name`)
+    REFERENCES `movies`.`movie_database` (`name_m`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_movie_collection_movie_database1`
+    FOREIGN KEY (`movie_release`)
+    REFERENCES `movies`.`movie_database` (`release_m`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+SET SQL_MODE = '';
+GRANT USAGE ON *.* TO user1;
+ DROP USER user1;
+SET SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+CREATE USER 'user1' IDENTIFIED BY 'password1';
+
+GRANT SELECT ON TABLE `movies`.* TO 'user1';
+GRANT SELECT, INSERT, TRIGGER ON TABLE `movies`.* TO 'user1';
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
